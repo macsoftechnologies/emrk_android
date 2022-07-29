f50cd232e1348bdc548a941d3f63f9784c98893a
Venky <babu.venkanna445@gmail.com>
2022-07-29 23:29:35 +0530

    design help

--- app/src/main/java/com/macsoftech/ekart/activities/DashboardActivity.java
+++ app/src/main/java/com/macsoftech/ekart/activities/DashboardActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -20,6 +21,7 @@ import butterknife.ButterKnife;
 
 public class DashboardActivity extends BaseActivity {
 
+    @Nullable
     @BindView(R.id.navigation)
     BottomNavigationView navigation;
 
--- app/src/main/java/com/macsoftech/ekart/fragments/HelpFragment.java
+++ app/src/main/java/com/macsoftech/ekart/fragments/HelpFragment.java
@@ -1,14 +1,26 @@
 package com.macsoftech.ekart.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.macsoftech.ekart.R;
+import com.macsoftech.ekart.activities.DashboardActivity;
+import com.macsoftech.ekart.activities.LoginActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Optional;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,6 +37,47 @@ public class HelpFragment extends BaseFragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+
+    @Nullable
+    @BindView(R.id.txt_header_1)
+    TextView txt_header_1;
+
+    @Nullable
+    @BindView(R.id.txt_header_2)
+    TextView txt_header_2;
+
+    @Nullable
+    @BindView(R.id.txt_header_3)
+    TextView txt_header_3;
+
+    @Nullable
+    @BindView(R.id.txt_header_4)
+    TextView txt_header_4;
+
+    @Nullable
+    @BindView(R.id.txt_header_5)
+    TextView txt_header_5;
+
+    @Nullable
+    @BindView(R.id.ll_content_1)
+    LinearLayout ll_content_1;
+
+    @Nullable
+    @BindView(R.id.ll_content_2)
+    LinearLayout ll_content_2;
+
+    @Nullable
+    @BindView(R.id.ll_content_3)
+    LinearLayout ll_content_3;
+
+    @Nullable
+    @BindView(R.id.ll_content_4)
+    LinearLayout ll_content_4;
+
+    @Nullable
+    @BindView(R.id.ll_content_5)
+    LinearLayout ll_content_5;
 
     public HelpFragment() {
         // Required empty public constructor
@@ -61,6 +114,109 @@ public class HelpFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_help, container, false);
+        View view= inflater.inflate(R.layout.fragment_help, container, false);
+        ButterKnife.bind(getActivity(),view);
+        return view;
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable  Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+      ll_content_1 =   view.findViewById(R.id.ll_content_1);
+      ll_content_2 =   view.findViewById(R.id.ll_content_2);
+      ll_content_3 =   view.findViewById(R.id.ll_content_3);
+      ll_content_4 =   view.findViewById(R.id.ll_content_4);
+      ll_content_5 =   view.findViewById(R.id.ll_content_5);
+        view.findViewById(R.id.txt_header_1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ontxtHeader1Click();
+            }
+        });
+
+
+        view.findViewById(R.id.txt_header_2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ontxtHeader2Click();
+            }
+        });
+
+
+        view.findViewById(R.id.txt_header_3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ontxtHeader3Click();
+            }
+        });
+
+        view.findViewById(R.id.txt_header_4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ontxtHeader4Click();
+            }
+        });
+
+        view.findViewById(R.id.txt_header_5).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ontxtHeader5Click();
+            }
+        });
+    }
+
+    @Optional
+    @OnClick(R.id.txt_header_1)
+    public void ontxtHeader1Click() {
+        ll_content_1.setVisibility(View.VISIBLE);
+        ll_content_2.setVisibility(View.GONE);
+        ll_content_3.setVisibility(View.GONE);
+        ll_content_4.setVisibility(View.GONE);
+        ll_content_5.setVisibility(View.GONE);
+    }
+
+
+    @Optional
+    @OnClick(R.id.txt_header_2)
+    public void ontxtHeader2Click() {
+        ll_content_1.setVisibility(View.GONE);
+        ll_content_2.setVisibility(View.VISIBLE);
+        ll_content_3.setVisibility(View.GONE);
+        ll_content_4.setVisibility(View.GONE);
+        ll_content_5.setVisibility(View.GONE);
+    }
+
+
+    @Optional
+    @OnClick(R.id.txt_header_3)
+    public void ontxtHeader3Click() {
+        ll_content_1.setVisibility(View.GONE);
+        ll_content_2.setVisibility(View.GONE);
+        ll_content_3.setVisibility(View.VISIBLE);
+        ll_content_4.setVisibility(View.GONE);
+        ll_content_5.setVisibility(View.GONE);
+    }
+
+    @Optional
+    @OnClick(R.id.txt_header_4)
+    public void ontxtHeader4Click() {
+        ll_content_1.setVisibility(View.GONE);
+        ll_content_2.setVisibility(View.GONE);
+        ll_content_3.setVisibility(View.GONE);
+        ll_content_4.setVisibility(View.VISIBLE);
+        ll_content_5.setVisibility(View.GONE);
+    }
+
+    @Optional
+    @OnClick(R.id.txt_header_5)
+    public void ontxtHeader5Click() {
+        ll_content_1.setVisibility(View.GONE);
+        ll_content_2.setVisibility(View.GONE);
+        ll_content_3.setVisibility(View.GONE);
+        ll_content_4.setVisibility(View.GONE);
+        ll_content_5.setVisibility(View.VISIBLE);
+    }
+
+
+
 }
